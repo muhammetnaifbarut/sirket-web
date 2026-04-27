4fdd94b2658f2883f1cba386217f4e453125f6a0
Muhammet Naif BARUT <muhammetnaifbarut@gmail.com>
2026-04-27 06:15:08 +0300

    fix: video showcase section gizlendi (hero ile icerik tekrar onlendi)

--- src/components/site/sections/VideoShowcaseSection.tsx
+++ src/components/site/sections/VideoShowcaseSection.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion'
 import { useState } from 'react'
-import HeroDashboard from '@/components/site/HeroDashboard'
+import HeroDashboardMobile from '@/components/site/HeroDashboardMobile'
 
 interface VideoShowcaseSectionProps {
   settings: Record<string, string>
